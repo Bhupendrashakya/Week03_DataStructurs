41f878a1414db490041e6fd76f06883396b0fa18
Bhupendra shakya <bhupendrashakya714@gmail.com>
2025-02-03 11:59:01 +0530

    Adding the program of sort sort employee ids using insertion sort

---
+++ src/com/tit/allsortingproblems/sortemployeeids/InsertionSort.java
@@ -0,0 +1,27 @@
+package com.tit.allsortingproblems.sortemployeeids;
+
+class InsertionSort
+{
+    // Method to perform Insertion Sort on the array of employee IDs
+    public void insertionSort(int[] ids) {
+        int n = ids.length;
+        for (int i = 1; i < n; i++) {
+            int key = ids[i];
+            int j = i - 1;
+            // Move elements of ids[0..i-1], that are greater than key, to one position ahead of their current position
+            while (j >= 0 && ids[j] > key) {
+                ids[j + 1] = ids[j];
+                j = j - 1;
+            }
+            ids[j + 1] = key;
+        }
+    }
+
+    // Method to print the array of employee IDs
+    public void printArray(int[] ids) {
+        for (int id : ids) {
+            System.out.print(id + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ src/com/tit/allsortingproblems/sortemployeeids/SortEmployeeIDs.java
@@ -0,0 +1,21 @@
+package com.tit.allsortingproblems.sortemployeeids;
+
+class SortEmployeeIDs
+{
+    public static void main(String[] args) {
+        // Array of employee IDs
+        int[] ids = {102, 54, 89, 32, 76, 100, 55,10,88,61,64,11,44,96,45};
+
+        // Create an instance of InsertionSort
+        InsertionSort sorter = new InsertionSort();
+
+        System.out.println("Original Employee IDs:");
+        sorter.printArray(ids);
+
+        // Perform Insertion Sort
+        sorter.insertionSort(ids);
+
+        System.out.println("Sorted Employee IDs:");
+        sorter.printArray(ids);
+    }
+}
